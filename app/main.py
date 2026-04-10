61c0e24106f66db28b52bc8595fe5be2539a3113
GitJaouad <jlaourayed@gmail.com>
2026-04-10 21:39:05 +0100

    Solution modifié

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     resultats_executions = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> func:
-        cle = (args, tuple(kwargs.items()))
+        sorted_kwargs = tuple(sorted(kwargs.items()))
+        cle = (args, sorted_kwargs)
         if cle not in resultats_executions:
             print("Calculating new result")
             resultats_executions[cle] = func(*args, **kwargs)
